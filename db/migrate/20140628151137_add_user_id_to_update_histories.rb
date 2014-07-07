a757d4d943b01fff77ba36c66c83af381ab9b047
kakusuke <bananafishmonger+github@gmail.com>
2014-07-08 01:04:04 +0900

    #40 fail to db:migrate

--- db/migrate/20140628151137_add_user_id_to_update_histories.rb
+++ db/migrate/20140628151137_add_user_id_to_update_histories.rb
@@ -1,7 +1,9 @@
 class AddUserIdToUpdateHistories < ActiveRecord::Migration
-  def change
-    change_table :update_histories do |t|
-      t.references :user, null: false, index: true, after: :article_id
-    end
+  def up
+    add_column :update_histories, :user_id, :integer, after: :article_id
+    change_column :update_histories, :user_id, :integer, references: :user, null: false, index: true
+  end
+  def down
+    remove_column :update_histories, :user_id
   end
 end
