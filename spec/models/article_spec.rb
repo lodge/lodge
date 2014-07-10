c61b8776ef9ba1bdc3efd35d357f59a1a34b0fcd
kakusuke <bananafishmonger+github@gmail.com>
2014-07-10 22:10:33 +0900

    add article method test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,11 +12,45 @@ RSpec.describe Article, :type => :model do
   it { should have_many(:stocked_users) }
   it { should have_many(:tags) }
 
-  context :create_history do
-    let(:edited) { FactoryGirl.create(:edited_article) }
+  describe :create_history do
+    let(:article) { FactoryGirl.create(:edited_article) }
 
-    it "should make update_history new" do
-      expect { edited.create_history }.to change(UpdateHistory, :count).by(1)
+    it "should create new update_history" do
+      expect { article.create_history }.to change(UpdateHistory, :count).by(1)
+    end
+    it "should be related with the new update_history" do
+      expect(article.create_history.article).to be_eql(article)
+    end
+  end
+
+  describe :save do
+    let(:article) { FactoryGirl.create(:edited_article) }
+    before do
+      article.title = "edited title"
+    end
+
+    context "when article is free" do
+      it "should update article" do
+        expect { article.save }.to change(article, :title_was).to("edited title")
+      end
+    end
+
+    context "when article is locked" do
+      before do
+        same_article = Article.find(article.id)
+        same_article.old_title = same_article.title
+        same_article.old_body = same_article.body
+        same_article.old_tags = same_article.tag_list.to_s
+        same_article.title = "new title"
+        same_article.body = "new body"
+        same_article.tag_list = "newtag1,newtag2"
+        same_article.save
+        article.save
+      end
+
+      it "should become error" do
+        expect(article.errors.size).to be_eql(1)
+      end
     end
   end
 end
