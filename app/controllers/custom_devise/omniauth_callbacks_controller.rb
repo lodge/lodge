b15bb6ea8e0accdf6e7ce8bb372aa966c455b63a
Daisuke Yamashita <dddaisuke@gmail.com>
2014-07-18 09:15:38 +0900

    マジックコードを追加

--- app/controllers/custom_devise/omniauth_callbacks_controller.rb
+++ app/controllers/custom_devise/omniauth_callbacks_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class CustomDevise::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.find_for_google_oauth2(request.env['omniauth.auth'])
