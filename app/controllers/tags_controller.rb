72885b4e120e3359388f2a941d40e55d2f91aa67
kakusuke <bananafishmonger+github@gmail.com>
2014-07-23 13:38:41 +0900

    fix tags pager

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   # GET /tags
   # GET /tags.json
   def index
-    @tags = Kaminari.paginate_array(Article.tag_counts.order(id: :desc)).page(params[:page]).per(PER_SIZE)
+    @tags = Article.tag_counts.page(params[:page]).per(PER_SIZE).order(id: :desc)
     @following_tags = current_user.following_tag_list
   end
 end
