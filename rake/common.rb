860654bde5bc06d7459d6a61b4f9d439f1d99d82
KOMATSU Seiji <skomatsu.comutt@gmail.com>
2014-08-01 01:20:10 +0900

    Moved methods into namespace

--- rake/common.rb
+++ rake/common.rb
@@ -1,14 +1,18 @@
-@rake_log = Logger.new(STDOUT)
-@rake_log.level = Logger::INFO
-@rake_log.formatter = proc { |severity, datetime, progname, msg|
-    "%s  %5s  %s\n" % [datetime.strftime('%Y-%m-%d %H:%M:%S,%L'), severity, msg]
-  }
-@root_path = File.expand_path('../..', __FILE__)
+namespace :lodge do
 
-def log_task_start(task_name)
-  @rake_log.info("Task `#{task_name}' started")
-end
+  @rake_log = Logger.new(STDOUT)
+  @rake_log.level = Logger::INFO
+  @rake_log.formatter = proc { |severity, datetime, progname, msg|
+      "%s  %5s  %s\n" % [datetime.strftime('%Y-%m-%d %H:%M:%S,%L'), severity, msg]
+    }
+  @root_path = File.expand_path('../..', __FILE__)
 
-def log_task_end(task_name)
-  @rake_log.info("Task `#{task_name}' succeeded")
+  def log_task_start(task_name)
+    @rake_log.info("Task `#{task_name}' started")
+  end
+
+  def log_task_end(task_name)
+    @rake_log.info("Task `#{task_name}' succeeded")
+  end
+
 end
