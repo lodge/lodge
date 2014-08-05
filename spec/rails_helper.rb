b784a73cb6d429efaf4b4dd053628d5d24807f7b
kakipo <kakipo@gmail.com>
2014-08-05 22:20:46 +0900

    Due to failure at Travis-CI, increased timeout threshold: 30 sec -> 60 sec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 Capybara.javascript_driver = :poltergeist
-
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, timeout: 60)
+end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
