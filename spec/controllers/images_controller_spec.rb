860d94134c47272d83832c9baf0aed238768d8d2
kakusuke <bananafishmonger+github@gmail.com>
2014-08-06 02:47:41 +0900

    fix for ruby1.9

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -10,21 +10,21 @@ RSpec.describe ImagesController, :type => :controller do
     before { request.accept = "application/json" }
     let(:image) { fixture_file_upload '/test.png', 'image/png' }
 
-    context '画像が0枚' do
+    context 'with no image' do
       let(:params) { { files: [] } }
       let(:result) { { error: 'image data not contain' }.to_json }
-      it 'エラーメッセージが返ってくる' do
+      it 'returns error message' do
         subject
         expect(response.body).to eq result
       end
     end
 
-    context '画像が1枚' do
+    context 'with a image' do
       let(:params) { { files: [image] } }
 
       it { expect(response).to be_success }
       it { expect(response.status).to eq(200) }
-      it '正しいjsonが返ってくる' do
+      it 'returns collect JSON' do
         subject
         json = JSON.parse(response.body)
         expect(json['image']['image']['image_url']) =~ /\/test.png\z/
@@ -32,13 +32,13 @@ RSpec.describe ImagesController, :type => :controller do
       it { expect { subject }.to change { Image.all.reload.count }.by(1) }
     end
 
-    context '画像が2枚' do
+    context 'with two images' do
       let(:params) { { files: [image, image2] } }
       let(:image2) { fixture_file_upload '/test2.png', 'image/png' }
 
       it { expect(response).to be_success }
       it { expect(response.status).to eq(200) }
-      it '正しいjsonが返ってくる' do
+      it 'returns collect JSON' do
         subject
         json = JSON.parse(response.body)
         expect(json['image']['image']['image_url']) =~ /\/test2.png\z/
