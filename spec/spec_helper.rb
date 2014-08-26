0dbcf1e142deb3d09a1ea7ddcff223ae51622241
kakusuke <bananafishmonger+github@gmail.com>
2014-08-27 00:59:09 +0900

    fix coveralls initialization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear!('rails')
 
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
