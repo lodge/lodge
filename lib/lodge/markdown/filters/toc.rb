252903b6d8ed274e3ae62c1f2e45e86c56c5888c
m-yamashita <mxyamashita@gmail.com>
2015-08-11 18:22:47 +0900

    Add pipeline filter for opening link with new browser tab.

--- lib/lodge/markdown/filters/toc.rb
+++ lib/lodge/markdown/filters/toc.rb
@@ -15,6 +15,16 @@ module Lodge
           doc.children << Nokogiri::HTML.fragment('<hr>')
         end
       end
+
+      class TargetBlankLink < HTML::Pipeline::Filter
+        def call
+          return if doc.children.blank?
+          doc.search('a').each do |a|
+            a['target'] = '_blank'
+          end
+          doc
+        end
+      end
     end
   end
 end
