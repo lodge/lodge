0eedde225e6717861f41e531e0a34f709c407543
uknmr <y.kinmori@gmail.com>
2016-03-18 18:01:20 +0900

    Declare 'shoulda-matchers' version.
    rspecが動作しなかったため、shoulda-matchersのバージョンを固定した。
    またShoulda::Matchers.configureも追加した。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -95,3 +95,10 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
