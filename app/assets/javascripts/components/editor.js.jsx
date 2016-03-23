02ab3866bcb37160cb70547bb17df1b5635a14b0
uknmr <y.kinmori@gmail.com>
2016-03-24 02:54:56 +0900

    Refactor Editor component to include Stateless component

--- app/assets/javascripts/components/editor.js.jsx
+++ app/assets/javascripts/components/editor.js.jsx
@@ -1,72 +1,51 @@
 class Editor extends React.Component {
   static propTypes = {
-    markdown: React.PropTypes.string,
-    id: React.PropTypes.string.isRequired,
-    nameAttr: React.PropTypes.string.isRequired
+    defaultValue: React.PropTypes.string,
+    objectName: React.PropTypes.string.isRequired,
+    fieldName: React.PropTypes.string.isRequired
   }
 
   state = {
-    markdown: this.props.markdown || ''
+    value: this.props.defaultValue || ''
   }
-  updateMarkdown = (markdown) => {
-    this.setState({ markdown: markdown });
+  handleTextChange = (e) => {
+    this.setState({ value: e.target.value });
   }
 
   render() {
-    let markdown = this.state.markdown;
+    let {value} = this.state;
     return (
       <div className="markdown-editor row">
-        <EditorMarkdown
-          id={ this.props.id }
-          nameAttr={ this.props.nameAttr }
-          onChange={ this.updateMarkdown }>
-          { markdown }
-        </EditorMarkdown>
-        <EditorPreview markdown={ markdown } />
+        <EditorMarkdown { ...this.props }
+          onChange={ this.handleTextChange }
+          value={ value } />
+        <EditorPreview value={ value } />
       </div>
     );
   }
 }
 
-class EditorMarkdown extends React.Component {
-  static propTypes = {
-    id: React.PropTypes.string.isRequired,
-    nameAttr: React.PropTypes.string.isRequired,
-    onChange: React.PropTypes.func.isRequired
-  }
+const EditorMarkdown = ({
+  value,
+  onChange,
+  objectName,
+  fieldName
+}) => (
+  <div className="col-xs-6">
+    <textarea
+      id={ `${objectName}_${fieldName}` }
+      className="editor-textarea form-control"
+      name={ `${objectName}[${fieldName}]` }
+      value={ value }
+      onChange={ onChange } />
+  </div>
+);
 
-  _onChange = (e) => {
-    this.props.onChange(e.target.value);
-  }
-
-  render() {
-    return (
-      <div className="col-xs-6">
-        <textarea
-          id={ this.props.id }
-          className="editor-textarea form-control"
-          name={ this.props.nameAttr }
-          value={ this.props.children }
-          onChange={ this._onChange } />
-      </div>
-    );
-  }
-}
-
-class EditorPreview extends React.Component {
-  static propTypes = {
-    markdown: React.PropTypes.string.isRequired
-  }
-
-  rawMarkup() {
-    return { __html: marked(this.props.markdown) };
-  }
-
-  render() {
-    return (
-      <div
-        className="markdown codehilite col-xs-6"
-        dangerouslySetInnerHTML={ this.rawMarkup() } />
-    );
-  }
-}
+const EditorPreview = ({
+  value
+}) => (
+  <div className="markdown codehilite col-xs-6"
+    dangerouslySetInnerHTML={
+      { __html: marked(value) }
+    } />
+);
