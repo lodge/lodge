66233cf238f1b654aa8367a1e774583a39ee4e7a
uknmr <y.kinmori@gmail.com>
2016-03-26 07:40:57 +0900

    Replace markdown parser with qiita-markdown

--- app/assets/javascripts/components/editor.js.jsx
+++ app/assets/javascripts/components/editor.js.jsx
@@ -2,50 +2,72 @@ class Editor extends React.Component {
   static propTypes = {
     defaultValue: React.PropTypes.string,
     objectName: React.PropTypes.string.isRequired,
-    fieldName: React.PropTypes.string.isRequired
+    fieldName: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired
+  }
+  state = { html: '' }
+  componentDidMount() {
+    this.preview(this.props.defaultValue);
   }
 
-  state = {
-    value: this.props.defaultValue || ''
+  handleChange = (e) => {
+    this.preview(e.target.value);
   }
-  handleTextChange = (e) => {
-    this.setState({ value: e.target.value });
+  compile = (rawMarkdown) => {
+    let token = document.querySelector('meta[name=csrf-token]').content;
+
+    return request.post(this.props.url, {
+      body: rawMarkdown,
+      authenticity_token: token
+    }, {
+      headers: {
+        'Accept': 'text/javascript'
+      }
+    });
+  }
+  preview = (rawMarkdown) => {
+    this.compile(rawMarkdown).then((response) => {
+      this.setState({ html: response.data });
+    })
+    .catch((response) => {
+      console.log(response);
+      this.setState({ html: rawMarkdown });
+    });
   }
 
   render() {
-    let {value} = this.state;
+    let { html } = this.state;
     return (
       <div className="markdown-editor row">
         <EditorMarkdown { ...this.props }
-          onChange={ this.handleTextChange }
-          value={ value } />
-        <EditorPreview value={ value } />
+          onChange={ this.handleChange } />
+        <EditorPreview html={ html } />
       </div>
     );
   }
 }
 
 const EditorMarkdown = ({
-  value,
   onChange,
   objectName,
-  fieldName
+  fieldName,
+  defaultValue
 }) => (
   <div className="col-xs-6">
     <textarea
       id={ `${objectName}_${fieldName}` }
       className="editor-textarea form-control"
       name={ `${objectName}[${fieldName}]` }
-      value={ value }
+      defaultValue={ defaultValue }
       onChange={ onChange } />
   </div>
 );
 
 const EditorPreview = ({
-  value
+  html
 }) => (
   <div className="markdown codehilite col-xs-6"
     dangerouslySetInnerHTML={
-      { __html: marked(value) }
+      { __html: html }
     } />
 );
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,7 @@
+Rails.application.assets.context_class.class_eval do
+  include Rails.application.routes.url_helpers
+end
+
 assets = Rails.application.config.assets
 assets.precompile += %w(ignore/*.css)
 assets.precompile += %w(externals/*.js)
