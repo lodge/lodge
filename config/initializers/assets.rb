ebcd9c30f398cf9373beda98eecdc18c8589167d
uknmr <y.kinmori@gmail.com>
2016-03-28 17:47:58 +0900

    Refactor assets initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,6 @@
-Rails.application.assets.context_class.class_eval do
-  include Rails.application.routes.url_helpers
-end
-
 assets = Rails.application.config.assets
 assets.precompile += %w(ignore/*.css)
 assets.precompile += %w(externals/*.js)
+assets.context_class.class_eval do
+  include Rails.application.routes.url_helpers
+end
