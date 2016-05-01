132e60e12ea119732c67defcd4b74b5a04ab7fdf
uknmr <y.kinmori@gmail.com>
2016-05-02 00:18:42 +0900

    Remove useless code :scream_cat:

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -95,10 +95,3 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
