ee10d2cbad024b75f7ad82fa8b6fcf264c9b2de7
m-yamashita <mxyamashita@gmail.com>
2017-10-09 17:11:14 +0900

    Fix deprecated code.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   #config.serve_static_assets = false
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
