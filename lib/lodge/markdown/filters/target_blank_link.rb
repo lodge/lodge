9333c7aca2bfd998dcb18f7d3003e2d5ab50d9da
m-yamashita <mxyamashita@gmail.com>
2017-10-29 23:38:19 +0900

    Open a new tab when click link to not self site domain.

--- lib/lodge/markdown/filters/target_blank_link.rb
+++ lib/lodge/markdown/filters/target_blank_link.rb
@@ -5,6 +5,7 @@ module Lodge
         def call
           return if doc.children.blank?
           doc.search('a').each do |a|
+            next if a['href'] =~ %r{\Ahttps?://#{ENV['LODGE_DOMAIN']}/|\A(.|/)}
             a['target'] = '_blank'
           end
           doc
